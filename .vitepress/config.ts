2e87db2193ec149d485b636c12972702e2a40674
lc <486836332@qq.com>
2023-06-27 23:18:36 +0800

    feat: update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,58 +1,10 @@
 import { defineConfig } from "vitepress";
+import menus from './menus'
 
-const menus = [
-  {
-    text: "前端",
-    collapsed: false,
-    items: [
-      { text: "HTML", link: "/page/client/html/" },
-      { text: "CSS", link: "/page/client/css/" },
-      { text: "JavaScript", link: "/page/client/javaScript/" },
-      { text: "TypeScript", link: "/page/client/typeScript/" },
-      { text: "Vue", link: "/page/client/vue/" },
-      { text: "React", link: "/page/client/react/" },
-      { text: "Webpack", link: "/page/client/webpack/" },
-      { text: "Vite", link: "/page/client/vite/" },
-    ],
-  },
-  {
-    text: "服务端",
-    collapsed: false,
-    items: [
-      {
-        text: "node",
-        items: [
-          { text: "express", link: "/page/service/node/express/" },
-          { text: "nest", link: "/page/service/node/nest/" },
-        ],
-      },
-    ],
-  },
-  {
-    text: "源码",
-    collapsed: false,
-    items: [
-      { text: "Vue2", link: "/page/code/vue2/" },
-      { text: "VueRouter", link: "/page/code/vueRouter/" },
-      { text: "Vuex", link: "/page/code/vuex/" },
-      { text: "Vue3", link: "/page/code/vue3/" },
-      { text: "Pinia", link: "/page/code/pinia/" },
-    ],
-  },
-  {
-    text: "其他",
-    collapsed: false,
-    items: [
-      { text: "git", link: "/page/other/git/" },
-      { text: "算法", link: "/page/other/algorithm/" },
-    ],
-  },
-];
-console.log(process.env.base)
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "mz-dfhp",
-  description: "A VitePress Site",
+  description: "blogs",
   base: process.env.base || "/",
   lang: 'zh-CN',
   // https://vitepress.dev/reference/default-theme-config
---
+++ .vitepress/menus.ts
@@ -0,0 +1,55 @@
+export default [
+  {
+    text: "前端",
+    collapsed: false,
+    items: [
+      { text: "HTML", link: "/page/client/html/" },
+      { text: "CSS", link: "/page/client/css/" },
+      { text: "JavaScript", link: "/page/client/javaScript/" },
+      { text: "TypeScript", link: "/page/client/typeScript/" },
+      { text: "Vue", link: "/page/client/vue/" },
+      { text: "React", link: "/page/client/react/" },
+      { text: "Webpack", link: "/page/client/webpack/" },
+      { text: "Vite", link: "/page/client/vite/" },
+    ],
+  },
+  {
+    text: "服务端",
+    collapsed: false,
+    items: [
+      {
+        text: "node",
+        items: [
+          { text: "express", link: "/page/service/node/express/" },
+          { text: "nest", link: "/page/service/node/nest/" },
+        ],
+      },
+    ],
+  },
+  {
+    text: "源码",
+    collapsed: false,
+    items: [
+      { text: "Vue2", link: "/page/code/vue2/" },
+      { text: "VueRouter", link: "/page/code/vueRouter/" },
+      { text: "Vuex", link: "/page/code/vuex/" },
+      { text: "Vue3", link: "/page/code/vue3/" },
+      { text: "Pinia", link: "/page/code/pinia/" },
+    ],
+  },
+  {
+    text: "其他",
+    collapsed: false,
+    items: [
+      { text: "git", link: "/page/other/git/" },
+      {
+        text: "leetcode",
+        collapsed: false,
+        items: [
+          { text: "1-50", link: "/page/other/leetcode/1-50" },
+          { text: "50-100", link: "/page/other/leetcode/50-100" },
+        ],
+      },
+    ],
+  },
+];
