a130e0dc7f43fa906ec697fddf158acb323b1fac
lc <486836332@qq.com>
2024-07-13 14:50:29 +0800

    feat: update

--- .vitepress/menus.ts
+++ .vitepress/menus.ts
@@ -3,16 +3,16 @@ export default [
     text: "前端",
     collapsed: false,
     items: [
-      { text: "HTML", link: "/page/client/HTML/" },
-      { text: "CSS", link: "/page/client/CSS/" },
-      { text: "JavaScript", link: "/page/client/JavaScript/" },
-      { text: "TypeScript", link: "/page/client/TypeScript/" },
-      { text: "Vue", link: "/page/client/Vue/" },
-      { text: "React", link: "/page/client/React/" },
-      { text: "Webpack", link: "/page/client/Webpack/" },
-      { text: "Vite", link: "/page/client/Vite/" },
-      { text: "Electron", link: "/page/client/Electron/" },
-      { text: "Flutter", link: "/page/client/Flutter/" },
+      { text: "HTML", link: "/page/client/html/" },
+      { text: "CSS", link: "/page/client/css/" },
+      { text: "JavaScript", link: "/page/client/javaScript/" },
+      { text: "TypeScript", link: "/page/client/typeScript/" },
+      { text: "Vue", link: "/page/client/vue/" },
+      { text: "React", link: "/page/client/react/" },
+      { text: "Webpack", link: "/page/client/webpack/" },
+      { text: "Vite", link: "/page/client/vite/" },
+      { text: "Electron", link: "/page/client/electron/" },
+      { text: "Flutter", link: "/page/client/flutter/" },
     ],
   },
   {
@@ -21,11 +21,11 @@ export default [
     items: [
       {
         text: "Node",
-        link: "/page/service/Node/"
+        link: "/page/service/node/"
       },
       {
         text: "Go",
-        link: "/page/service/Go/"
+        link: "/page/service/go/"
       },
     ],
   },
@@ -33,26 +33,26 @@ export default [
     text: "数据库",
     collapsed: false,
     items: [
-      { text: "MongoDB", link: "/page/database/MongoDB/" },
-      { text: "MySQL", link: "/page/database/MySQL/" },
+      { text: "MongoDB", link: "/page/database/mongoDB/" },
+      { text: "MySQL", link: "/page/database/mySQL/" },
     ],
   },
   {
     text: "源码",
     collapsed: false,
     items: [
-      { text: "Vue2", link: "/page/code/Vue2/" },
-      { text: "VueRouter", link: "/page/code/VueRouter/" },
-      { text: "Vuex", link: "/page/code/Vuex/" },
-      { text: "Vue3", link: "/page/code/Vue3/" },
-      { text: "Pinia", link: "/page/code/Pinia/" },
+      { text: "Vue2", link: "/page/code/vue2/" },
+      { text: "VueRouter", link: "/page/code/vueRouter/" },
+      { text: "Vuex", link: "/page/code/vuex/" },
+      { text: "Vue3", link: "/page/code/vue3/" },
+      { text: "Pinia", link: "/page/code/pinia/" },
     ],
   },
   {
     text: "其他",
     collapsed: false,
     items: [
-      { text: "Git", link: "/page/other/Git/" },
+      { text: "Git", link: "/page/other/git/" },
       { text: "算法", link: "/page/other/algorithm/common"}
     ],
   },
---
+++ page/service/go/src/01.hello.go
@@ -0,0 +1,7 @@
+package main // 包名
+import "fmt" // 导入包
+
+// 主函数
+func main()  {
+	fmt.Println("hello world")
+}
