bd9c2ec750bcd3b3b6e06962eb7a1dc21a65f67a
lc <486836332@qq.com>
2024-07-14 22:45:02 +0800

    feat: update

--- page/service/go/src/01.hello.go
+++ page/service/go/src/01.hello.go
@@ -1,7 +1,39 @@
 package main // 包名
 import "fmt" // 导入包
 
+var g1 = 1
+var g2 = 2
+
+var (
+	g3 = 3
+	g4 = 4
+)
+
 // 主函数
-func main()  {
+func main() {
 	fmt.Println("hello world")
+
+	var age int = 18
+	fmt.Println("age:", age)
+
+	var age1 int
+	fmt.Println("age1:", age1) // 0
+
+	var age2 = 18
+	fmt.Println("age2:", age2)
+
+	name := "mz"
+	fmt.Println("name:", name)
+
+	var n1, n2, n3 int
+	fmt.Println(n1, n2, n3)
+
+	var n4, n5, n6 int = 4, 5, 6
+	fmt.Println(n4, n5, n6)
+
+	n7, n8 := 7, 8
+	fmt.Println(n7, n8)
+
+	fmt.Println(g1, g2, g3, g4)
+
 }
